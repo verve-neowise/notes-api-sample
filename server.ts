6800190154906c30cc34e8efe313101fd80b9b4c
verve-neowise <verve.neowise@gmail.com>
2023-03-09 21:25:58 +0500

    inital commit

---
+++ server.ts
@@ -0,0 +1,55 @@
+import express from 'express'
+import cors from 'cors'
+import { ACCESS_TOKEN, PORT } from './config'
+
+import { PrismaClient } from '@prisma/client'
+
+const app = express()
+const prisma = new PrismaClient()
+
+app.use(cors({
+    origin: '*',
+    allowedHeaders: '*'
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+type Note = {
+    title: string,
+    content: string
+}
+
+app.use((req, res, next) => {
+    const token = req.header('x-access-token')
+
+    if (!token || token != ACCESS_TOKEN) {
+        return res
+            .status(400)
+            .send("Access denied")
+    }
+    next()
+})
+
+app.post('/notes', async (req, res) => {
+    const body: Note = req.body
+    
+    const result = await prisma.notes.create({
+        data: {
+            title: body.title,
+            content: body.content
+        }
+    })
+
+    res.json(result)
+})
+
+app.get('/notes', async (req, res) => {
+    
+    const result = await prisma.notes.findMany()
+    res.json(result)
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT)
+})
